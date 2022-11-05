5eabb55449ca6420c1b45673d1008a721239789b
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-11-05 08:35:44 +0200

    dfr

--- src/index.js
+++ src/index.js
@@ -16,22 +16,25 @@ trendingList.then(res =>
 );
 // console.log('fgtryhju');
 
+//////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 trendingList.then(res => {
   const id = res[1].id;
   const details = apiRequest.fetchMoviesDetails(id);
-  details.then(res => console.log(res.data));
+  details.then(res => console.log('Movie Details', res.data));
 });
+
+//////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 apiRequest.searchQuery = 'titanic';
 const search = apiRequest.fetchSearchMovie();
-
 search.then(res => render(res));
 
 function render(obj) {
   const img = document.querySelector('.movie');
-  const marcup = `<img src="https://image.tmdb.org/t/p/w500${obj[0].poster_path}" alt="poster path of ${obj[0].title}">
+  const markup = `<img src="https://image.tmdb.org/t/p/w500${obj[0].poster_path}" alt="poster path of ${obj[0].title}">
 	<img src="https://image.tmdb.org/t/p/w300${obj[0].backdrop_path}" alt="poster path of ${obj[0].title}">
 		<img src="https://image.tmdb.org/t/p/w500${obj[0].backdrop_path}" alt="poster path of ${obj[0].title}">
 		<img src="https://image.tmdb.org/t/p/w1280${obj[0].backdrop_path}" alt="poster path of ${obj[0].title}">`;
-  img.insertAdjacentHTML('beforeend', marcup);
+  img.insertAdjacentHTML('beforeend', markup);
 }
