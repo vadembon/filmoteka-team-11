3b8744a3450f76678ba51a1442ad320d9a96c8ec
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-11-08 10:23:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import FilmApiService from './js/movie_database_api';
-// import { render } from './js/example';
+import { render } from './js/example';
 // import renderModal from './templates/modal.hbs';
 import renderMovieList from './js/render_movie_list';
 
 import { refs } from './js/refs';
 import * as footer_modal from './js/footer_modal';
-
 
 import idToGenre from './js/idToGenre';
 import renderModal from './js/render_film_modal';
