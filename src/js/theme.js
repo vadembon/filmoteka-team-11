39844b5838aff437ec67e551c92c59bfa06e777e
Kyrylo Dyachenko <mallet25460@gmail.com>
2022-11-10 20:26:12 +0200

    icons

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,17 +1,22 @@
+import { refs } from './refs';
+
+
+
+
 function setLightTheme() {
-    document.documentElement.style.setProperty('--bg-color', '#FCFCFC');
-    document.documentElement.style.setProperty('--bg-text', '#202025');
-    document.documentElement.style.setProperty('--modal-text-color', '#5f6775');
+    refs.body.setProperty(`background-color` `white`)
+    // document.documentElement.style.setProperty('--bg-text', '#202025');
+    // document.documentElement.style.setProperty('--modal-text-color', '#5f6775');
     saveThemeFavorites('Light');
   }
   function setDarkTheme() {
-    document.documentElement.style.setProperty('--bg-color', '#202025');
-    document.documentElement.style.setProperty('--bg-text', '#FCFCFC');
-    document.documentElement.style.setProperty('--modal-text-color', '#FCFCFC');
+    refs.body.setProperty(`background-color` `#1b080a`)
+    // document.documentElement.style.setProperty('--bg-text', '#FCFCFC');
+    // document.documentElement.style.setProperty('--modal-text-color', '#FCFCFC');
     saveThemeFavorites('Dark');
   }
   
-  export function setTheme(theme) {
+  export function setTheme() {
     switch (theme) {
       case 'Light':
         el.toggleSwitch.checked = false;
