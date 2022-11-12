b2f9e53430ca6f8d66e5b7cf3604f6788de1f6ce
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-11-13 01:37:42 +0200

    бекдропик)

--- src/js/modal_film_cart.js
+++ src/js/modal_film_cart.js
@@ -1,82 +1,28 @@
-// (() => {
-//     const refss = {
-//     //   openModalBtn: document.querySelector(".card__link"),
-//       closeModalBtn: document.querySelector("[data-modal-close]"),
-//       modal: document.querySelector("[data-modal]")
-//     };
-
-//     // refss.openModalBtn.addEventListener("click", toggleModal);
-//     refss.closeModalBtn.addEventListener("click", toggleModal);
-
-//     function toggleModal() {
-//       refss.modal.classList.toggle("is-hidden");
-//     }
-//   })();
-
-//   const btn0 = document.querySelector('.watch_btn > span');
-// btn0.addEventListener('click', function(){
-//   btn0.innerHTML =
-//     (btn0.innerHTML === 'add to Watched') ? btn0.innerHTML = 'Remove?' : btn0.innerHTML = 'add to Watched';
-// })
-
-// //изминение надписей в кнопках при клике
-// const btn1 = document.querySelectorAll('.queue_btn > span');
-// for (let i = 0; i < btn1.length; i++) {
-
-//   btn1[i].addEventListener('click', function() {
-//     this.innerHTML =
-//       (this.innerHTML === 'add to queue') ? this.innerHTML = 'Remove?' : this.innerHTML = 'add to queue';
-//   })
-
-// }
-
-// // Закрытие по клику вне поля модального окна
-// const backdropik = document.querySelector('.backdrop')
-// const box = document.querySelector('.modal-container');
-// document.addEventListener('click', (e)=>{
-// const click = e.composedPath().includes(box,backdropik);
-// if (!click) {
-//     box.style.display = 'none';
-//     backdropik.style.display = 'none';
-// }
-// }'
-
-///////////\\\\\\\\\\
 import { refs } from './refs';
 import FilmApiService from './movie_database_api';
-refs.backdrop.classList.add('visually-hidden');
+const apiRequest = new FilmApiService();
 
 refs.closeModalBtn.addEventListener('click', toggleModal);
+
 function toggleModal() {
   refs.backdrop.classList.add('visually-hidden');
 }
-const apiRequest = new FilmApiService();
+
 refs.filmList.addEventListener('click', onClick);
+
 function onClick(evt) {
   evt.preventDefault();
-  console.log(evt.path[2].id);
+  console.log(evt.path[2]);
   const details = apiRequest.fetchMoviesDetails(evt.path[2].id);
-  details.then(res => console.log(res));
+  details.then(res =>
+    refs.backdrop.setAttribute(
+      'style',
+      `background-image: url("https://image.tmdb.org/t/p/original/${res.backdrop_path}"); background-size: cover; background-position: 50% 50%;`
+    )
+  );
   refs.backdrop.classList.remove('visually-hidden');
-  // refs.modalСontainer.removeAttribute(‘hidden’);
 }
-const btn0 = document.querySelector('.watch_btn > span');
-btn0.addEventListener('click', function () {
-  btn0.innerHTML =
-    btn0.innerHTML === 'add to Watched'
-      ? (btn0.innerHTML = 'Remove?')
-      : (btn0.innerHTML = 'add to Watched');
-});
-//изминение надписей в кнопках при клике
-const btn1 = document.querySelectorAll('.queue_btn > span');
-for (let i = 0; i < btn1.length; i++) {
-  btn1[i].addEventListener('click', function () {
-    this.innerHTML =
-      this.innerHTML === 'add to queue'
-        ? (this.innerHTML = 'Remove?')
-        : (this.innerHTML = 'add to queue');
-  });
-}
+
 // Закрытие по клику вне поля модального окна
 // const backdropik = document.querySelector(‘.backdrop’);
 // const box = document.querySelector(‘.modal-container’);
