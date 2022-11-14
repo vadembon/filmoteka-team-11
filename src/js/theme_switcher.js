4f7fae6a78c8dcc81c18ed7768b8efcd3bcdd041
Vadym Bondarenko <vadem.bondarenko@gmail.com>
2022-11-14 14:08:25 +0200

    theams

--- src/js/theme_switcher.js
+++ src/js/theme_switcher.js
@@ -21,6 +21,13 @@ function themeToggleHandler(evt) {
 
 export function lightTheme() {
   refs.themeToggle.removeAttribute('checked');
+
+  refs.watchBtn.classList.remove('dark');
+  refs.queueBtn.classList.remove('dark');
+  refs.closeModalBtn.classList.remove('dark');
+  refs.modalContainer.classList.remove('dark');
+  refs.containerGallery.classList.remove('dark');
+  refs.footer.classList.remove('dark');
   refs.body.classList.remove('dark');
   refs.filmList.classList.remove('dark');
 
@@ -30,6 +37,12 @@ export function lightTheme() {
 export function darkTheme() {
   refs.themeToggle.setAttribute('checked', 'checked');
   // refs.body.classList.remove('light');
+  refs.watchBtn.classList.add('dark');
+  refs.queueBtn.classList.add('dark');
+  refs.closeModalBtn.classList.add('dark');
+  refs.modalContainer.classList.add('dark');
+  refs.containerGallery.classList.add('dark');
+  refs.footer.classList.add('dark');
   refs.body.classList.add('dark');
   refs.filmList.classList.add('dark');
 }
